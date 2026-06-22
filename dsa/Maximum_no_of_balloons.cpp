d92f264c34c9c93998a127ee8e3048503dd759b4
agrimagoel30 <agrimagoel30@gmail.com>
2026-06-22 22:07:51 +0530

    added updated file

---
+++ dsa/Maximum_no_of_balloons.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int maxNumberOfBalloons(string text) {
+        vector<int>mp(26,0);
+      for(char ch:text){
+        mp[ch-'a']++;
+      }
+      return min({mp['b'-'a'],mp['a'-'a'],mp['l'-'a']/2,mp['o'-'a']/2,mp['n'-'a']});
+    }
+};
