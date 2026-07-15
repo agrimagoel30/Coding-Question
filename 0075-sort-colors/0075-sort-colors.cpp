e2fd9521428e60c25d7e97a8322260bb9d966d64
Agrima Goel <163877502+agrimagoel30@users.noreply.github.com>
2026-07-15 14:04:12 +0530

    Time: 0 ms (100%), Space: 11.8 MB (11.68%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        // int red=0;
+        // int white=0;
+        // int blue=0;
+        // for(int i=0;i<nums.size();i++){
+        //     if(nums[i]==)
+        // }
+        sort(nums.begin(),nums.end());
+    }
+
+};
