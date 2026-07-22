ffaa2232b0345bb8a7e452f41a53a93b08048773
Agrima Goel <163877502+agrimagoel30@users.noreply.github.com>
2026-07-23 00:34:13 +0530

    Time: 0 ms (100%), Space: 22.5 MB (98.29%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,13 +1,21 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        int i=1;
-        while(i<nums.size()){
-            if(nums[i]==nums[i-1])
-            nums.erase(nums.begin()+i);
-            else
-            i++;
+        // int i=1;
+        // while(i<nums.size()){
+        //     if(nums[i]==nums[i-1])
+        //     nums.erase(nums.begin()+i);
+        //     else
+        //     i++;
+        // }
+        // return nums.size();
+        int i=0;
+        for(int j=1;j<nums.size();j++){
+            if(nums[j]!=nums[i]){
+                i++;
+                nums[i]=nums[j];
+            }
         }
-        return nums.size();
+        return i+1;
     }
 };
