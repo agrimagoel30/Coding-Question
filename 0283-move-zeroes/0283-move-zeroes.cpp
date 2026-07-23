0af19b1fc40bbf63cb3c610ff864c6e1327e6dbf
Agrima Goel <163877502+agrimagoel30@users.noreply.github.com>
2026-07-23 11:34:26 +0530

    Time: 0 ms (100%), Space: 23.9 MB (56.38%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,15 +1,25 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
+        // int i=0;
+        // int n=nums.size();
+        // while(i<n){
+        //     if(nums[i]==0){
+        //     nums.erase(nums.begin()+i);
+        //     nums.push_back(0);
+        //     n--;
+        //     }
+        //     else
+        //     i++;
+        // }
         int i=0;
+        int j=0;
         int n=nums.size();
         while(i<n){
-            if(nums[i]==0){
-            nums.erase(nums.begin()+i);
-            nums.push_back(0);
-            n--;
+            if(nums[i]!=0){
+              swap(nums[i],nums[j]);
+                j++;
             }
-            else
             i++;
         }
     }
